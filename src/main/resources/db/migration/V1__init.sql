8a62f8faf5190b4d7de84d3550629325d7de6020
skahn <d1215@naver.com>
2023-05-30 23:13:32 +0900

    first commit

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,20 @@
+CREATE TABLE product
+(
+    id   BIGINT       NOT NULL auto_increment,
+    name VARCHAR(255) NOT NULL,
+    price Decimal(64, 0)  NOT NULL,
+    quantity INTEGER  NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE order_product
+(
+    id         BIGINT          NOT NULL AUTO_INCREMENT,
+    product_id BIGINT          NOT NULL,
+    PRIMARY KEY (id)
+);
+
+alter table order_product
+    add constraint order_product_fk
+        foreign key (product_id)
+            references product (id);
